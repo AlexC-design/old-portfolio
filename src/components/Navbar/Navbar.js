9a903901b10d726ba9a02d2843e2aede35b2da67
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-01-09 14:53:30 +0000

    adding functionality to the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
 
 import "./navbar.css";
 
-export default class Navbar extends Component {
+class Navbar extends Component {
+  pathCheck = path =>
+    this.props.location.pathname === path ? "active" : "inactive";
+
   render() {
     return (
       <div className="navbar-container">
         <ul className="navbar">
-          <li>About</li>
-          <li>Home</li>
-          <li>Contact</li>
+          <Link to="/" className={`navbar-link ${this.pathCheck("/")}`}>
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className={`navbar-link ${this.pathCheck("/about")}`}
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className={`navbar-link ${this.pathCheck("/contact")}`}
+          >
+            Contact
+          </Link>
         </ul>
       </div>
     );
   }
 }
+
+const wrappedComponent = withRouter(Navbar);
+
+export default wrappedComponent;
