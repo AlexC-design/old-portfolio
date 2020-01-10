0faa0c9f2bb7fbcde17047c3f96b2992f5a36ec2
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-01-10 13:00:16 +0000

    creating buttons section component and adding it to the project display section

--- src/components/ProjectsSection/ProjectButtons/ProjectButtons.js
+++ src/components/ProjectsSection/ProjectButtons/ProjectButtons.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+import './project-buttons.css'
+
 export default class ProjectButtons extends Component {
   render() {
     return (
-      <div>
-        <div></div>
+      <div className='buttons-container'>
+        {this.props.children}
       </div>
     );
   }
--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -4,13 +4,17 @@ import { ProjectsDisplay } from "./ProjectsDisplay/ProjectsDisplay";
 import ProjectCard from "../ProjectsSection/ProjectCard/ProjectCard";
 
 import "./css/projects-section.css";
+import { MainButton } from "../MainButton/MainButton";
 
 export default function ProjectsSection(props) {
   const { projects } = props;
 
   return (
     <div className="projects-section">
-      <ProjectButtons />
+      <ProjectButtons>
+        <MainButton text='Websites' color='primary' filled={true} />
+        <MainButton text='UI / UX' color='primary' filled={false} />
+      </ProjectButtons>
       <ProjectsDisplay>
         {projects.map(project => (
           <ProjectCard project={project} key={project.name} />
