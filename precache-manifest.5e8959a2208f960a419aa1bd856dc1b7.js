a6ece17345519e9a9c85a99b3efa47d54a761867
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-01-21 16:37:53 +0000

    Updates

---
+++ precache-manifest.5e8959a2208f960a419aa1bd856dc1b7.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39a6a2008f5fca98aac4ba985cc9a92",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "89fc0da1226e9c5c227c",
+    "url": "/portfolio-website/static/css/main.7f386e6b.chunk.css"
+  },
+  {
+    "revision": "79347fcc1533405006f8",
+    "url": "/portfolio-website/static/js/2.94d243b6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio-website/static/js/2.94d243b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89fc0da1226e9c5c227c",
+    "url": "/portfolio-website/static/js/main.53c07f65.chunk.js"
+  },
+  {
+    "revision": "2517a8b4628c70fa0925",
+    "url": "/portfolio-website/static/js/runtime-main.6a698c4e.js"
+  },
+  {
+    "revision": "fc31d670b5a691f8f3d42ad482317f1b",
+    "url": "/portfolio-website/static/media/ae.fc31d670.svg"
+  },
+  {
+    "revision": "1fc28d6acecd71e9c7a4848db195bd81",
+    "url": "/portfolio-website/static/media/ai.1fc28d6a.svg"
+  },
+  {
+    "revision": "3f40e54ff2db5634166b973c8efeee6d",
+    "url": "/portfolio-website/static/media/css.3f40e54f.svg"
+  },
+  {
+    "revision": "fca0b973eeb152da6a292e12b0fad37d",
+    "url": "/portfolio-website/static/media/figma.fca0b973.svg"
+  },
+  {
+    "revision": "e0b9d0f032806a6a58186606ed1c44c3",
+    "url": "/portfolio-website/static/media/fo1.e0b9d0f0.png"
+  },
+  {
+    "revision": "59a38cecb924f1ca181e050cd2bc2cfd",
+    "url": "/portfolio-website/static/media/fo2.59a38cec.png"
+  },
+  {
+    "revision": "ed8a78c7ea5d119f2bbe98614046f0d8",
+    "url": "/portfolio-website/static/media/fo3.ed8a78c7.png"
+  },
+  {
+    "revision": "589f70b2f4fb3e67120274f7f835c340",
+    "url": "/portfolio-website/static/media/fo4.589f70b2.png"
+  },
+  {
+    "revision": "27a784fceb7eae5915785e77d35f0c87",
+    "url": "/portfolio-website/static/media/github-icon.27a784fc.svg"
+  },
+  {
+    "revision": "1040296fc9f8b85cb6c7d1052af731fa",
+    "url": "/portfolio-website/static/media/html.1040296f.svg"
+  },
+  {
+    "revision": "6d626fd141d5b35630f8062a71de6201",
+    "url": "/portfolio-website/static/media/js.6d626fd1.svg"
+  },
+  {
+    "revision": "b08f359265f842c63d1560043d5aa295",
+    "url": "/portfolio-website/static/media/line-of-action-app-logo.b08f3592.png"
+  },
+  {
+    "revision": "5b0b493476356a804d10ea5296ab2072",
+    "url": "/portfolio-website/static/media/line-of-action-website-logo.5b0b4934.png"
+  },
+  {
+    "revision": "9ce47756ceb54e5cc13378a172ad88bd",
+    "url": "/portfolio-website/static/media/linkedin-icon.9ce47756.svg"
+  },
+  {
+    "revision": "f96069c2cf9eb535c5d3f5c40d5d6bbe",
+    "url": "/portfolio-website/static/media/loaa1.f96069c2.png"
+  },
+  {
+    "revision": "64d177325ce60701e964f5ecc56aead2",
+    "url": "/portfolio-website/static/media/loaa2.64d17732.png"
+  },
+  {
+    "revision": "9edc7d831a86866350d35c3c2f5fbbe2",
+    "url": "/portfolio-website/static/media/loaa3.9edc7d83.png"
+  },
+  {
+    "revision": "f1b61f26fc9f054e956c0072307e0a76",
+    "url": "/portfolio-website/static/media/loaw1.f1b61f26.png"
+  },
+  {
+    "revision": "2b4a36a7490f16251d4614d18263cf43",
+    "url": "/portfolio-website/static/media/loaw2.2b4a36a7.png"
+  },
+  {
+    "revision": "b7c9410987aa6a89bb3915f3719c10cb",
+    "url": "/portfolio-website/static/media/loaw3.b7c94109.png"
+  },
+  {
+    "revision": "5f977a4b573d21cf42eedbbfa35221d9",
+    "url": "/portfolio-website/static/media/mail-handle.5f977a4b.png"
+  },
+  {
+    "revision": "9a3b4df2c4a082f3700245a933fdcaab",
+    "url": "/portfolio-website/static/media/mc1.9a3b4df2.png"
+  },
+  {
+    "revision": "0fdc754ad53ec4ac3e8f97c036f4ce03",
+    "url": "/portfolio-website/static/media/mc2.0fdc754a.png"
+  },
+  {
+    "revision": "fc38df50163d5f8a6d6a37f3b8976e08",
+    "url": "/portfolio-website/static/media/mc3.fc38df50.png"
+  },
+  {
+    "revision": "914a3520c4d00544cb2f2326a886f0e5",
+    "url": "/portfolio-website/static/media/mc4.914a3520.png"
+  },
+  {
+    "revision": "640faf2736267985863f655d7a8ef5a8",
+    "url": "/portfolio-website/static/media/movie-cluster-logo.640faf27.png"
+  },
+  {
+    "revision": "9dc9cae3672605f0493fe1b31a7cd06d",
+    "url": "/portfolio-website/static/media/pg1.9dc9cae3.png"
+  },
+  {
+    "revision": "df39ed603ae62fd02a1b3e428fd04ee7",
+    "url": "/portfolio-website/static/media/pg2.df39ed60.png"
+  },
+  {
+    "revision": "d0a04a8373756be91e29eddbee5a37fa",
+    "url": "/portfolio-website/static/media/pg3.d0a04a83.png"
+  },
+  {
+    "revision": "e1b6f92f1dfc95860aed69e24b03a5c3",
+    "url": "/portfolio-website/static/media/pg4.e1b6f92f.png"
+  },
+  {
+    "revision": "1ba204ba63af668716d2240ccf96d0b2",
+    "url": "/portfolio-website/static/media/phone-sound-right.1ba204ba.png"
+  },
+  {
+    "revision": "7e6ab311fc35839ffad43b309714fecc",
+    "url": "/portfolio-website/static/media/portfolio-logo.7e6ab311.png"
+  },
+  {
+    "revision": "38233b5e8eed6b8d1ae10960233524cd",
+    "url": "/portfolio-website/static/media/primal-gym-logo.38233b5e.png"
+  },
+  {
+    "revision": "795b60611028b4a62577e97d475336c5",
+    "url": "/portfolio-website/static/media/ps.795b6061.svg"
+  },
+  {
+    "revision": "7fb151564a90a3b2e853baed495a043f",
+    "url": "/portfolio-website/static/media/react.7fb15156.svg"
+  },
+  {
+    "revision": "630359d864a60c838fa6ce65d2cb927a",
+    "url": "/portfolio-website/static/media/redux.630359d8.svg"
+  },
+  {
+    "revision": "63d3d978c5b06e8ec77922367997f2d7",
+    "url": "/portfolio-website/static/media/take-a-seat-logo.63d3d978.png"
+  },
+  {
+    "revision": "ab7858576a4888766fb0189eb5561ee3",
+    "url": "/portfolio-website/static/media/tas1.ab785857.png"
+  },
+  {
+    "revision": "7a40fc84e161e3f64c903d25bf18313c",
+    "url": "/portfolio-website/static/media/tas2.7a40fc84.png"
+  },
+  {
+    "revision": "208943476fcedd29fa871345cf2d533a",
+    "url": "/portfolio-website/static/media/tas3.20894347.png"
+  },
+  {
+    "revision": "be73b7b0abce69f2380e8b7c89592373",
+    "url": "/portfolio-website/static/media/tr1.be73b7b0.png"
+  },
+  {
+    "revision": "1ce4f9e970e7a83ee4305a232b7089b7",
+    "url": "/portfolio-website/static/media/tr2.1ce4f9e9.png"
+  },
+  {
+    "revision": "c0c7288c997b3d0636adfc8a89e48271",
+    "url": "/portfolio-website/static/media/tr3.c0c7288c.png"
+  },
+  {
+    "revision": "18720db03239fd80046b09f38681b637",
+    "url": "/portfolio-website/static/media/tr4.18720db0.png"
+  },
+  {
+    "revision": "5e3db17c7ef7e73196815fa3c5b8fb44",
+    "url": "/portfolio-website/static/media/xd.5e3db17c.svg"
+  }
+]);
